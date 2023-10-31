43c3da2d7974bedf6d6e9bc31007eac3adfb2acf
fringemonkey89 <markomajeticjr@gmail.com>
2023-10-31 19:01:00 -0400

    added more code to server.js

--- server.js
+++ server.js
@@ -11,4 +11,26 @@ app.use(express.static(__dirname + '/public'));
 
 io.sockets.on('error', e => console.log(e))
 
+io.sockets.on('connection', socket => {
+    socket.on('broadcaster', () => {
+        broadcaster = socket.id;
+        socket.broadcaster.emit('broadcaster')
+    })
+    socket.on('watcher', () => {
+        socket.io(broadcaster).emit('watcher', socket.id)
+    });
+    socket.on('offer', (id, message) => {
+        socket.io(id).emit('offer', socket.id, message)
+    });
+    socket.on('answer', (id, message) => {
+        socket.io(id).emit('answer', socket.id, message)
+    });
+    socket.on('candidate', (id, message) => {
+        socket.io(id).emit('candidate', socket.id, message)
+    });
+    socket.on('disconnect', () => {
+        socket.io(broadcaster).emit('disconnectPeer', socket.id)
+    });
+})
+
 server.listen(port, () => console.log(`server is running on: ${port}`))
